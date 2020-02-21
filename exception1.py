a9598a531e67fd224019ba69dca9ad93d9d44f1b
Artur Ivanov <ivanov.arthur13@gmail.com>
2020-02-21 19:01:22 +0300

    Решение

--- exception1.py
+++ exception1.py
@@ -10,11 +10,17 @@
     
 """
 
+create_dict = {'Как дела?': 'Хорошо!', 'Что делаешь?': 'Программирую.', 'Нравится?': 'Да.'}
+print('Введите ваш вопрос:')
 def ask_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+  try:
+    while True:
+      user_say = input()
+      if user_say in create_dict.keys():
+          print(create_dict[user_say])
+      else:
+          print('Вопрос неверный.')
+  except KeyboardInterrupt:
+      print('\n''Пока!')
 if __name__ == "__main__":
     ask_user()
