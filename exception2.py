11ad157f16aa979df2a74677d3c564c76b6e4aa8
Artur Ivanov <ivanov.arthur13@gmail.com>
2020-02-21 19:40:05 +0300

    Решение

--- exception2.py
+++ exception2.py
@@ -13,14 +13,16 @@
 """
 
 def get_summ(num_one, num_two):
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+  try:
+      num_one = int(input('Первое число: '))
+      num_two = int(input('Второе число: '))
+      return num_one + num_two
+  except ValueError:
+    return('Вводить можно только числа!')
+
 if __name__ == "__main__":
-    print(get_summ(2, 2))
-    print(get_summ(3, "3"))
-    print(get_summ("4", "4"))
-    print(get_summ("five", 5))
-    print(get_summ("six", "шесть"))
+   print(get_summ(2, 2))
+   print(get_summ(3, "3"))
+   print(get_summ("4", "4"))
+   print(get_summ("five", 5))
+   print(get_summ("six", "шесть"))
