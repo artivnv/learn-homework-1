8047167c3c0f50290b244f91d378e10ea2e1f16d
Artur Ivanov <ivanov.arthur13@gmail.com>
2020-02-21 15:19:14 +0300

    Решение

--- for.py
+++ for.py
@@ -1,21 +1,37 @@
 """
-
 Домашнее задание №1
 
 Цикл for: Оценки
 
-* Создать список из словарей с оценками учеников разных классов 
+* Создать список из словарей с оценками учеников разных классов
   школы вида [{'school_class': '4a', 'scores': [3,4,4,5,2]}, ...]
 * Посчитать и вывести средний балл по всей школе.
 * Посчитать и вывести средний балл по каждому классу.
 """
+school_list = [
+{'school_class': '4a', 'scores': [5,4,4,5,2]},
+{'school_class': '4b', 'scores': [3,4,5,5,2]},
+{'school_class': '5a', 'scores': [5,5,4,5,3]},
+{'school_class': '5b', 'scores': [1,3,4,2,3]},
+{'school_class': '6b', 'scores': [5,3,4,5,5]},
+]
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-    
-if __name__ == "__main__":
-    main()
+for group in school_list:
+    sum_score = 0
+    sum_group = 0
+    all_school = 0
+    for score in group['scores']:
+        sum_score += score
+        sum_group += 1
+        all_school += (sum_score/sum_group)
+    print(f'Средний балл по классу {group["school_class"]}: {sum_score/sum_group}')
+print(f'Средний балл по всей школе: {all_school/sum_group}')
+
+
+
+
+
+
+
+
+
